4edcb119e412aef93e28f0acdec60ac1da1e1b0a
henriquepablo <pablo269704@gmail.com>
2024-03-24 15:07:13 -0300

    feat: creating structure body and adding style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, TextInput, Image } from 'react-native';
+import { View, Text, TextInput, Image, ScrollView } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import styles from '../styles/style';
 import { useState } from 'react';
 
@@ -16,19 +17,81 @@ const HomeScreen = () => {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
+      <ScrollView>
         <View style={styles.header}>
           <TextInput placeholder='Pesquise por uma localidade' style={style}  onFocus={custmOnFocus} onBlur={custmOnBlur}/>
           <Image source={require('../imgs/search.png')} style={styles.iconSearch}/>
           <Image source={require('../imgs/circle-x.png')} style={styles.iconCircleX} />
         </View>
         
+        <View style={styles.body}> 
+          <LinearGradient colors={['#1786ED', '#91DCFF']} style={styles.gradient} start={{x:0.5, y:0}}>
+              <View>
+                <View style={styles.ViewLocalidade}>
+                  <Image source={require('../imgs/map-pin.png')} style={styles.imageMap}/>
+                  <Text style={styles.textLocalidade}>São Paulo</Text>
+                </View>
+                <Text style={styles.infoData}>18 de março de 2024 às 19:30</Text>
+              </View>
+
+              <View style={styles.viewTemperatura}>
+                  <Image source={require('../imgs/sol.png')}/>
+                  <Text style={styles.temperatura}>28°C</Text>
+                  <Text style={styles.sensacao}>Sensação térmica de 34º</Text>
+              </View>
+
+              <View style={styles.viewCard}>
+                  <View style={styles.card}>
+                    <Image source={require('../imgs/wind.png')}/>
+                    <Text style={styles.dataApi}>5Km/h</Text>
+                    <Text style={styles.textCard}>Velocidade Vento</Text>
+                  </View>
+
+                  <View style={styles.card}>
+                    <Image source={require('../imgs/waves.png')}/>
+                    <Text style={styles.dataApi}>6%</Text>
+                    <Text style={styles.textCard}>Umidade</Text>
+                  </View>
+
+                  <View style={styles.card}>
+                    <Image source={require('../imgs/cloud.png')}/>
+                    <Text style={styles.dataApi}>50%</Text>
+                    <Text style={styles.textCard}>% de chuva</Text>
+                  </View>
+
+                  <View style={styles.card}>
+                    <Image source={require('../imgs/eye.png')}/>
+                    <Text style={styles.dataApi}>5Km</Text>
+                    <Text style={styles.textCard}>Visiblidade</Text>
+                  </View>
+              </View>
+          </LinearGradient>
+
+          <View style={styles.viewInfo}>
+              <View style={styles.lines}>
+                  <Image source={require('../imgs/emoji_nature.png')}/>
+                  <Text style={{color: '#3D3D3D'}}>Pólen: Nenhum</Text>
+              </View>
+
+              <View style={styles.lines}>
+                  <Image source={require('../imgs/windAQI.png')}/>
+                  <Text style={{color: '#3D3D3D'}}>AQI: Insalubre 187</Text>
+              </View>
+
+              <View style={styles.lines}>
+                  <Image source={require('../imgs/circle-gauge.png')}/>
+                  <Text style={{color: '#3D3D3D'}}>Corrida: Baixo</Text>
+              </View>
+              
+          </View>
+        </View>
 
 
 
 
 
-
+        </ScrollView>
     </View>
   );
 };
--- src/styles/style.js
+++ src/styles/style.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff'
+    },
     header: {
         backgroundColor: '#1786ED',
         height: 120,
@@ -34,6 +37,97 @@ const styles = StyleSheet.create({
         padding: 9,
         marginRight: -320,
         marginTop: -18
+    },
+    body: {
+        height: 697,
+    },
+    gradient: {
+        height: 480
+    },
+    ViewLocalidade: {
+        flexDirection: 'row',
+        marginLeft: 20,
+        height: 36,
+        alignContent: 'center'
+    },
+    textLocalidade: {
+        fontSize: 24,
+        color: '#fff',
+        marginLeft: 10
+    },
+    imageMap: {
+        height: 24,
+        marginTop: 5
+    },
+    infoData: {
+        color: '#fff',
+        fontSize: 14,
+        marginLeft: 20,
+        fontWeight: '400',
+    },
+    viewTemperatura: {
+        alignItems: 'center',
+        marginTop: 15
+    },
+    temperatura: {
+        fontSize: 42,
+        color: '#fff',
+        fontWeight: '600'
+    },
+    sensacao: {
+        fontSize: 19,
+        color: '#E7E7E7',
+        marginTop: 12
+    },
+    viewCard: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 24,
+        flexDirection: 'row',
+        gap: 9,
+        flexWrap: 'wrap'
+    },  
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        height: 116,
+        width: 87,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+    dataApi: {
+        color: '#454545',
+        margin: 6
+    },
+    textCard: {
+        textAlign: 'center',
+        color: '#888888'
+    },
+    viewInfo: {
+        borderStyle: 'solid',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 366,
+        height: 138,
+        borderWidth: 1,
+        borderColor: '#D1D1D1',
+        marginTop: 30,
+        borderRadius: 8,
+    },
+    lines: {
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        height: 30,
+        width: 342,
+        borderRadius: 6,
+        borderColor: '#D1D1D1',
+        alignItems:'center',
+        padding: 4,
+        gap: 10,
+        margin: 4.5
     }
 
 });
